401761a6dc95f5c2f7719c5cc4d26db01e53aa7e
tallesaragao <tallesaragao@brq.com>
2022-08-30 14:34:56 -0300

    wip: implementação inicial hash

--- Api/WeatherForecast.cs
+++ Api/WeatherForecast.cs
@@ -14,4 +14,5 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
+    // public string Hash { get; set; }
 }
